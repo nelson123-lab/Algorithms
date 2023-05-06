ebf23942c5bd0ab9a1e60f5b22053c84d99e5e09
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-05-05 23:41:35 -0500

    Update Binary_search.py

--- Binary_search.py
+++ Binary_search.py
@@ -1,12 +1,23 @@
 def search(self, nums, target):
-        low = 0
-        high = len(nums)-1
-        while low <= high:
-            mid = (high + low) // 2
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] > target:
-                high = mid - 1
-            else:
-                low = mid + 1
-        return -1
+    # Set the initial values for the low and high indices
+    low = 0
+    high = len(nums)-1
+    
+    # While the low index is less than or equal to the high index
+    while low <= high:
+        # Calculate the middle index
+        mid = (high + low) // 2
+        
+        # If the middle element is the target, return its index
+        if nums[mid] == target:
+            return mid
+        # If the middle element is greater than the target, search the left half
+        elif nums[mid] > target:
+            high = mid - 1
+        # If the middle element is less than the target, search the right half
+        else:
+            low = mid + 1
+    
+    # If the target is not found, return -1
+    return -1
+
